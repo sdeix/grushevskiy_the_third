3249fbebbe9887b995a469ba204fd9cf0643f030
sdeix <you@example.com>
2024-11-08 01:22:42 +0700

    38 страница

--- pop-it-mvc/core/bootstrap.php
+++ pop-it-mvc/core/bootstrap.php
@@ -2,18 +2,22 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
-//Добавляем пользовательскую функцию автозагрузки классов
-spl_autoload_register(function ($className) {
-   $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-   $className = str_replace('\\', '/', $className);
+//Подключение автозагрузчика composer
+require_once __DIR__ . '/../vendor/autoload.php';
 
-   foreach ($paths['classes'] as $path) {
-       $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-       if (file_exists($fileName)) {
-           require_once $fileName;
-       }
-   }
-});
+spl_autoload_register(function ($className) {
+    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+    $className = str_replace('\\', '/', $className);
+ 
+    foreach ($paths['classes'] as $path) {
+        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
+        if (file_exists($fileName)) {
+            require_once $fileName;
+        }
+    }
+ });
+ 
+
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
@@ -28,8 +32,7 @@ function getConfigs(string $path = DIR_CONFIG): array
    return $settings;
 }
 
+
 require_once __DIR__ . '/../routes/web.php';
 return new Src\Application(new Src\Settings(getConfigs()));
-
-
 
