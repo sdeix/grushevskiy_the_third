8b04f8bf3dd563680d8065775b01f9c98dc47a3d
sdeix <you@example.com>
2024-11-13 04:25:34 +0700

    финиш

--- pop-it-mvc/config/app.php
+++ pop-it-mvc/config/app.php
@@ -8,20 +8,22 @@ return [
     'routeMiddleware' => [
         'auth' => \Middlewares\AuthMiddleware::class,
     ],
-'routeAppMiddleware' => [
-   'csrf' => \Middlewares\CSRFMiddleware::class,
-   'trim' => \Middlewares\TrimMiddleware::class,
-   'specialChars' => \Middlewares\SpecialCharsMiddleware::class
-],
+    'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
+    ],
+
     'validators' => [
         'required' => \Validators\RequireValidator::class,
         'unique' => \Validators\UniqueValidator::class
     ],
     'providers' => [
-   'kernel' => \Providers\KernelProvider::class,
-   'route' => \Providers\RouteProvider::class,
-   'db' => \Providers\DBProvider::class,
-   'auth' => \Providers\AuthProvider::class,
-],
+        'kernel' => \Providers\KernelProvider::class,
+        'route' => \Providers\RouteProvider::class,
+        'db' => \Providers\DBProvider::class,
+        'auth' => \Providers\AuthProvider::class,
+    ],
 
 ];
